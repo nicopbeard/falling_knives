cf71b3d93eba1dfd1430cb722999927489ff0c41
nicopbeard <npb221@lehigh.edu>
2020-04-05 15:29:27 -0700

    updated database.py

--- database.py
+++ database.py
@@ -1,18 +1,42 @@
-from google.cloud import bigquery
+import datetime
+import logging
+import os
+from flask import Flask, render_template, request, Response
+import sqlalchemy
+# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+# something like https://cloud.google.com/kms/ to help keep secrets secret.
+db_user = "postgres"
+db_pass = "csb312"
+db_name = "database"
+cloud_sql_connection_name = "adroit-nimbus-272020:us-central1:database"
 
-def export_items_to_bigquery():
-    # Instantiates a client
-    bigquery_client = bigquery.Client()
+app = Flask(__name__)
 
-    # Prepares a reference to the dataset
-    dataset_ref = bigquery_client.dataset('my_datasset_id')
+logger = logging.getLogger()
 
-    table_ref = dataset_ref.table('my_table_id')
-    table = bigquery_client.get_table(table_ref)  # API call
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # postgres+pg8000://<db_user>:<db_pass>@/<db_name>?unix_sock=/cloudsql/<cloud_sql_instance_name>/.s.PGSQL.5432
+    sqlalchemy.engine.url.URL(
+        drivername='postgres+pg8000',
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={'unix_sock': '/cloudsql/{}/.s.PGSQL.5432'.format(cloud_sql_connection_name)
+        }
+    ),
+    # ... Specify additional properties here.
+    # ...
+)
 
-    rows_to_insert = [
-        (u'Phred Phlyntstone', 32),
-        (u'Wylma Phlyntstone', 29),
-    ]
-    errors = bigquery_client.insert_rows(table, rows_to_insert)  # API request
-    assert errors == []
+
+@app.route('/', methods=['GET'])
+def index():
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        query = conn.execute("SELECT * FROM company").fetchall()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
