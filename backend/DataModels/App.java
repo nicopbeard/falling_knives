ac80aa0cdb9d1d7bfb0999624532233b772bfd2e
nicopbeard <npb221@lehigh.edu>
2020-04-21 21:10:15 -0700

    created user interface for falling knife

--- backend/DataModels/App.java
+++ backend/DataModels/App.java
@@ -1,44 +1,77 @@
 import java.sql.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class App
 {
     public static void main(String args[]) throws SQLException, IOException, java.lang.ClassNotFoundException
     {
-        Connection c = null;
-        try
-        {
-           Class.forName("org.postgresql.Driver");
-           c = DriverManager.getConnection("jdbc:postgresql:///postgres?cloudSqlInstance=adroit-nimbus-272020:us-central1:database&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=csb312");
-        }
-        catch (Exception e)
-        {
-           e.printStackTrace();
-           System.err.println(e.getClass().getName()+": "+e.getMessage());
-           System.exit(0);
-        }
-        System.out.println("Opened database successfully");
+         Connection c = null;
+         try
+         {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql:///postgres?cloudSqlInstance=adroit-nimbus-272020:us-central1:database&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=csb312");
+         }
+         catch (Exception e)
+         {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+         }
+         System.out.println("Opened database successfully");
 
-        Statement statement = c.createStatement();
-        String query = "select * from company;";
-        ResultSet result = null;
-        try
-        {
-            result = statement.executeQuery(query);
-            if(!result.next())
-            {
-               System.out.println("That table doesn't exist");
-            }
-            System.out.println(String.format("%-10s %-40s %-15s %-10s", "Ticker", "Name", "Public_Data", "Sector"));
+         PreparedStatement statement;
+         ResultSet result = null;
+         try
+         {
+            Scanner input = new Scanner(System.in);
+            boolean flag = true;
+            System.out.println("------------------------------");
+            System.out.println("Welcome to the user interface");
             do
             {
-               System.out.println(String.format("%-10s %-40s %-15s %-10s", result.getString("ticker"), result.getString("name"), result.getString("public_data"), result.getString("sector")));
+               System.out.println("Please enter the start date, end date, and percent change");
+               System.out.print("Start Date: ");
+               String startDate = input.next();
+               System.out.print("End Date: ");
+               String endDate = input.next();
+               System.out.print("Percent change: ");
+               while(!input.hasNextFloat())
+               {
+                  input.next();
+                  System.out.println("Incorrect input, try again");
+               }
+               float percentChange = input.nextFloat();
+
+               statement = c.prepareStatement("select dataid, sum((cast(open as float)-cast(close as float))/nullif(cast(open as float), 0))  as change, '" + startDate + "'  as startdate, '" + endDate + "' as enddate from market where date between '" + startDate + "' and '" + endDate + "' group by dataid having sum((cast(open as float)-cast(close as float))/nullif(cast(open as float), 0)) < " + percentChange + ";");
+
+               result = statement.executeQuery();
+               if(!result.next())
+               {
+                  System.out.println("That table doesn't exist");
+               }
+               System.out.println(String.format("%-7s %-10s %-25s %-15s %-15s", "Ticker", "Data ID", "Change", "Start Date", "End Date"));
+               do
+               {
+                  PreparedStatement ticker = c.prepareStatement("select ticker from relates where dataid = '" + result.getString("dataid") + "'");
+                  ResultSet tickerResult = ticker.executeQuery();
+                  tickerResult.next();
+                  System.out.println(String.format("%-7s %-10s %-25s %-15s %-15s", tickerResult.getString("ticker"), result.getString("dataid"), result.getString("change"), result.getString("startdate"), result.getString("enddate")));
+               }
+               while(result.next());
+
+               System.out.println("Enter \"quit\" to quit or anything else to search again");
+               String choice = input.next();
+               if(choice.equals("quit"))
+               {
+                  flag = false;
+               }
             }
-            while(result.next());
-        }
-        catch(Exception e)
-        {
-           e.printStackTrace();
-        }
-     }
+            while(flag);
+         }
+         catch(Exception e)
+         {
+            e.printStackTrace();
+         }
+      }
 }
