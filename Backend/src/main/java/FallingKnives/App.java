6d4bb5bd31409589602ed96f8c4c6be6eaa875d0
nicopbeard <npb221@lehigh.edu>
2020-09-07 20:18:58 -0400

    updating backend to handle api requests

--- Backend/src/main/java/FallingKnives/App.java
+++ Backend/src/main/java/FallingKnives/App.java
@@ -1,15 +1,15 @@
 package FallingKnives;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.sql.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Scanner;
+import static spark.Spark.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main(String[] args) throws SQLException
     {
@@ -23,7 +23,22 @@ public class App
             System.out.println("Connenction Failed");
             e.printStackTrace();
         }
+        regularUI(conn);
+    }
 
+    public static void webUI(Connection conn)
+    {
+        // Get the port on which to listen for requests
+        Spark.port(getIntFromEnv("PORT", 4567));
+        // Set up a route for serving the main page
+        get("/", (request, response) -> {
+
+            return "";
+        });
+    }
+
+    public static void regularUI(Connection conn) throws SQLException
+    {
         menu();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         char toDo = prompt(in);
@@ -32,7 +47,63 @@ public class App
             Tester.TestMethod(conn);
         }
         else if (toDo == 'A') {
-            // TODO: Launch Application
+            PreparedStatement statement;
+            ResultSet result = null;
+            try
+            {
+                Scanner input = new Scanner(System.in);
+                boolean flag = true;
+                System.out.println("------------------------------");
+                System.out.println("Welcome to the user interface");
+                do
+                {
+                    System.out.println("Please enter the start date, end date, and percent change");
+                    System.out.print("Start Date (mm-dd-yyyy): ");
+                    String startDate = input.next();
+                    System.out.print("End Date (mm-dd-yyyy): ");
+                    String endDate = input.next();
+                    System.out.print("Percent change: ");
+                while(!input.hasNextFloat())
+                {
+                    input.next();
+                    System.out.println("Incorrect input, try again");
+                }
+                float percentChange = input.nextFloat();
+
+                statement = conn.prepareStatement("select dataid, sum((close-open)/nullif(open, 0))  as change, '" + startDate + "'  as startdate, '" + endDate + "' as enddate from market where date between '" + startDate + "' and '" + endDate + "' group by dataid having sum((close-open)/nullif(open, 0)) < " + percentChange + ";");
+
+                result = statement.executeQuery();
+                if(!result.next())
+                {
+                    System.out.println("That table doesn't exist");
+                }
+                PrintStream o = new PrintStream(new File("output.csv"));
+                PrintStream console = System.out;
+                System.setOut(o);
+                System.out.println(String.format("%-7s %-10s %-25s %-15s %-15s", "Ticker", "Data ID", "Change", "Start Date", "End Date"));
+                do
+                {
+                    PreparedStatement ticker = conn.prepareStatement("select ticker from relates where dataid = '" + result.getString("dataid") + "'");
+                    ResultSet tickerResult = ticker.executeQuery();
+                    tickerResult.next();
+                    System.out.println(String.format("%-7s %-10s %-25s %-15s %-15s", tickerResult.getString("ticker"), result.getString("dataid"), result.getString("change"), result.getString("startdate"), result.getString("enddate")));
+                }
+                while(result.next());
+
+                System.setOut(console);
+                System.out.println("Enter \"quit\" to quit or anything else to search again");
+                String choice = input.next();
+                if(choice.equals("quit"))
+                {
+                    flag = false;
+                }
+                }
+                while(flag);
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
         }
         else if (toDo == 'C') {
             PrevCloseView.CreatePrevCloseView(conn);
