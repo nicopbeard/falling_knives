aec18a10b9b329502f6064947c92b6cfb3d369b7
nicopbeard <npb221@lehigh.edu>
2020-09-07 21:35:55 -0400

    working on frontend

---
+++ web/app.js
@@ -0,0 +1,25 @@
+// // Create a request variable and assign a new XMLHttpRequest object to it.
+// var request = new XMLHttpRequest()
+
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+var sDate = document.getElementById("sdate")
+var eDate = document.getElementById("edate")
+var pChange = document.getElementById("pchange")
+
+// request.onload = function () {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response)
+
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach((movie) => {
+//       console.log(movie.title)
+//     })
+//   } else {
+//     console.log('error')
+//   }
+// }
+
+// // Send request
+// request.send()
