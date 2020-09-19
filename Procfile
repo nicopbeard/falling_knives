1cd7dfd716f2f6743e8fdd3c56e14d5422dd713b
nicopbeard <npb221@lehigh.edu>
2020-09-19 19:34:22 -0400

    updating

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -cp target/classes/FallingKnives:src/main/java/FallingKnives/* FallingKnives.App
+web: java -cp target/classes:src/main/java/FallingKnives/* FallingKnives.App
--- src/main/java/FallingKnives/App.java
+++ src/main/java/FallingKnives/App.java
@@ -20,7 +20,7 @@ public class App
             conn = DriverManager.getConnection("jdbc:postgresql:///postgres?cloudSqlInstance=adroit-nimbus-272020:us-central1:database&socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=postgres&password=csb312");
         }
         catch (Exception e) {
-            System.out.println("Connenction Failed");
+            System.out.println("Connection Failed");
             e.printStackTrace();
         }
         regularUI(conn);
